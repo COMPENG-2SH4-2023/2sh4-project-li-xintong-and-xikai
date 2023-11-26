f23f1e30e8585aa9398f36e542415f62e250c534
xu503 <xu503@mcmaster>
2023-11-25 23:54:28 -0500

    message

--- GameMechs.cpp
+++ GameMechs.cpp
@@ -2,52 +2,74 @@
 
 GameMechs::GameMechs()
 {
-
+    input = 0;
+    score = 0;
+    exitFlag = false;
+    loseflag = false;
+    boardSizeX = 30;
+    boardSizeY = 15;
 }
 
-GameMechs::GameMechs(int boardX, int boardY)
+GameMechs::GameMechs(int boardX, int boardY) //used as a setter
 {
-
+    input = 0;
+    score = 0;
+    exitFlag = false;
+    loseflag = false;
+    boardSizeX = boardX;
+    boardSizeY = boardY;
 }
 
 // do you need a destructor?
-
+GameMechs::~GameMechs(){
+    
+}
 
 
 bool GameMechs::getExitFlagStatus()
 {
+    return exitFlag;
+}
 
+bool GameMechs::getloseflagStatus()
+{
+    return loseflag = true;
 }
 
 char GameMechs::getInput()
 {
-
+    return input;
 }
 
 int GameMechs::getBoardSizeX()
 {
-
+    return boardSizeX;
 }
 
 int GameMechs::getBoardSizeY()
 {
-
+    return boardSizeY;
 }
 
 
 void GameMechs::setExitTrue()
 {
+    exitFlag = true;
+}
 
+void GameMechs::setloseflagTrue()
+{
+    loseflag = true;
 }
 
 void GameMechs::setInput(char this_input)
 {
-
+    input = this_input; //deep copy
 }
 
 void GameMechs::clearInput()
 {
-
+    input = NULL;
 }
 
 
--- Player.cpp
+++ Player.cpp
@@ -1,30 +1,33 @@
 #include "Player.h"
 #include "MacUILib.h"
 
-Player::Player(GameMechs* thisGMRef)
+Player::Player(GameMechs* thisGMRef) //can been seen as the initial construct function
 {
-    mainGameMechsRef = thisGMRef;
+    playerPos.x = thisGMRef -> getBoardSizeX();
+    playerPos.y = thisGMRef -> getBoardSizeY();
+    playerPos.symbol = '*';
     myDir = STOP;
     // more actions to be included
-    // creat a fsm:
+    mainGameMechsRef = thisGMRef;
 }
 
 Player::~Player()
 {   
-    delete mainGameMechsRef;
+    delete mainGameMechsRef; //only thing include private that might required to delete.
     // delete any heap members here
 }
 
-void Player::getPlayerPos(objPos &returnPos)
+void Player::getPlayerPos(objPos &returnPos) //the snake head location, always print add very beginning first of the element.
 {
-    
-    // return the reference to the playerPos arrray list
+    returnPos.x = playerPos.x; //intergrate with both ability
+    returnPos.y = playerPos.y; //deep copy direct pass the value to the 
+    // return the reference to the playerPos array list
 }
 
-void Player::updatePlayerDir() // CONNECTED WITH THE PLAY FUNCTION 
+void Player::updatePlayerDir() // CONNECTED WITH THE PLAY FUNCTION FIXED 
 {
-    if(MacUILib_hasChar()){
-        char p_temp = MacUILib_getChar();    
+    if(mainGameMechsRef->getInput() != NULL){//check the mainGamemechref weather have neo content
+        char p_temp = mainGameMechsRef -> getInput(); // pass the reference     
         if(p_temp == (('w') || ('W'))&&(myDir != DOWN)){
             myDir = UP;
         }
@@ -41,14 +44,39 @@ void Player::updatePlayerDir() // CONNECTED WITH THE PLAY FUNCTION
             myDir = STOP;
         }
     }
+    //myDir == NULL; // CLEAR 
     // PPA3 input processing logic        
 }
 
-void Player::movePlayer()
+void Player::movePlayer() //fixed 
 {
+    //myDir from input of analysitic 
+
     switch(myDir != STOP){
         case UP:
-         
+            playerPos.y--;
+            if(playerPos.y == 0){
+                playerPos.y = 14;
+            }
+            break;
+        case DOWN:
+            playerPos.y++;
+            if(playerPos.y == 15){
+                playerPos.y = 1;
+            }
+            break;
+        case LEFT:
+            playerPos.x--;
+            if(playerPos.x == 0){
+                playerPos.x = 29;
+            }
+            break;
+        case RIGHT:
+            playerPos.x++;
+            if(playerPos.x == 30){
+                playerPos.x = 1;
+            }
+            break;
     }
     // PPA3 Finite State Machine logic
 }
